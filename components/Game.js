9e67bcc9cf3d50d489dc887433022b0b3495b5a7
dchan331 <dchan331@gmail.com>
2017-08-08 21:26:55 -0700

    added game component

--- components/Game.js
+++ components/Game.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      numbers: [1,2,6,4]
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Game Component</Text>
+        {this.state.numbers.map((num) => {
+          return (
+            <TouchableOpacity style={styles.square}>
+              <View style={styles.number}>
+                <Text style={{fontSize: 30}}>{num}</Text>
+              </View>
+            </TouchableOpacity>
+        );
+        })}
       </View>
     );
   }
@@ -18,4 +32,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  square:{
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    width: 100,
+    height: 100
+  },
+  number: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
 });
