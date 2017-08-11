ce2b39d04480c46666c029a05f0a877d7fe536d3
dchan331 <dchan331@gmail.com>
2017-08-11 10:37:45 -0700

    game logic all good

--- components/Game.js
+++ components/Game.js
@@ -73,7 +73,7 @@ export default class Game extends React.Component {
       temp[1] = this.state.operators[num]
       this.setState({clicked: temp})
     }else if(temp.length === 0 || this.state.numbers.indexOf(temp[temp.length - 1][0]) === -1){
-        alert('choose a number')
+      alert('choose a number')
     }else{
       var newClicked = temp.concat([this.state.operators[num]])
       this.setState({clicked: newClicked})
@@ -111,73 +111,73 @@ export default class Game extends React.Component {
             </TouchableOpacity>
           }
           {this.state.numbers[1] === '' ? <View style={styles.square}></View>:
-            <TouchableOpacity style={styles.square} onPress={() => this.handleNum(1)}>
-              <View style={styles.number}>
-                <Text style={{fontSize: 30}}>{this.state.numbers[1]}</Text>
-              </View>
-            </TouchableOpacity>
-          }
-          </View>
-          <View>
-            {this.state.numbers[2] === '' ? <View style={styles.square}></View>:
-            <TouchableOpacity style={styles.square} onPress={() => this.handleNum(2)}>
-              <View style={styles.number}>
-                <Text style={{fontSize: 30}}>{this.state.numbers[2]}</Text>
-              </View>
-            </TouchableOpacity>
-          }
-          {this.state.numbers[3] === '' ? <View style={styles.square}></View>:
-            <TouchableOpacity style={styles.square} onPress={() => this.handleNum(3)}>
-              <View style={styles.number}>
-                <Text style={{fontSize: 30}}>{this.state.numbers[3]}</Text>
-              </View>
-            </TouchableOpacity>
-          }
-          </View>
-        </View>
-        {/* end of container for nums */}
-        <View style={styles.operatorsView}>
-          {this.state.operators.map((ops,i) => {
-            return (
-              <TouchableOpacity key={i} style={styles.square} onPress={() => this.handleSign(i)}>
-                <View style={styles.operators}>
-                  <Text style={{fontSize: 30}}>{ops}</Text>
-                </View>
-              </TouchableOpacity>
-            );
-          })}
-        </View>
-
-        <TouchableOpacity style={styles.square} onPress={() => this.handleUndo()}>
-          <View><Text>Undo </Text><Text>{this.state.clicked}</Text></View>
-        </TouchableOpacity>
-
-        <View style={styles.operatorsView}>
-          {this.state.solutions.length !== 0 ?
-            <View>
-              {this.state.solutions.map((solve, index) => {
-                return <Text key={index}>{solve}</Text>
-              })}
-              <TouchableOpacity onPress={() => this.handleSolutions('hide')}><Text>Hide Solutions</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.square} onPress={() => this.handleNum(1)}>
+            <View style={styles.number}>
+              <Text style={{fontSize: 30}}>{this.state.numbers[1]}</Text>
             </View>
-            :
-            <TouchableOpacity style={styles.container} onPress={() => this.handleSolutions()}>
-              <View><Text>Solutions</Text></View>
-            </TouchableOpacity>
-          }
-        </View>
+          </TouchableOpacity>
+        }
       </View>
+      <View>
+        {this.state.numbers[2] === '' ? <View style={styles.square}></View>:
+        <TouchableOpacity style={styles.square} onPress={() => this.handleNum(2)}>
+          <View style={styles.number}>
+            <Text style={{fontSize: 30}}>{this.state.numbers[2]}</Text>
+          </View>
+        </TouchableOpacity>
+      }
+      {this.state.numbers[3] === '' ? <View style={styles.square}></View>:
+      <TouchableOpacity style={styles.square} onPress={() => this.handleNum(3)}>
+        <View style={styles.number}>
+          <Text style={{fontSize: 30}}>{this.state.numbers[3]}</Text>
+        </View>
+      </TouchableOpacity>
+    }
+  </View>
+</View>
+{/* end of container for nums */}
+<View style={styles.operatorsView}>
+  {this.state.operators.map((ops,i) => {
+    return (
+      <TouchableOpacity key={i} style={styles.square} onPress={() => this.handleSign(i)}>
+        <View style={styles.operators}>
+          <Text style={{fontSize: 30}}>{ops}</Text>
+        </View>
+      </TouchableOpacity>
     );
+  })}
+</View>
+
+<TouchableOpacity style={styles.square} onPress={() => this.handleUndo()}>
+  <View><Text>Undo </Text><Text>{this.state.clicked}</Text></View>
+</TouchableOpacity>
+
+<View style={styles.operatorsView}>
+  {this.state.solutions.length !== 0 ?
+    <View>
+      {this.state.solutions.map((solve, index) => {
+        return <Text key={index}>{solve}</Text>
+      })}
+      <TouchableOpacity onPress={() => this.handleSolutions('hide')}><Text>Hide Solutions</Text></TouchableOpacity>
+    </View>
+    :
+    <TouchableOpacity style={styles.container} onPress={() => this.handleSolutions()}>
+      <View><Text>Solutions</Text></View>
+    </TouchableOpacity>
   }
+</View>
+</View>
+);
+}
 }
 
 
 const math_it_up = {
-    '+': function (x, y) { return x + y },
-    '-': function (x, y) { return x - y },
-    '/': function (x, y) { return (x / y)},
-    'x': function (x, y) { return x * y },
-  }
+  '+': function (x, y) { return x + y },
+  '-': function (x, y) { return x - y },
+  '/': function (x, y) { return (x / y)},
+  'x': function (x, y) { return x * y },
+}
 
 function calculator(array){
   var len = array.length
@@ -190,49 +190,63 @@ function calculator(array){
   var num2 = typeof index1 === "object" ? index2 : eval(index2);
   console.log('nums', num1, num2);
   let answer;
-  if(operator === 'x' || operator === '/'){
-    console.log('0');
-    if(typeof num1 === "object" && typeof num2 !== "object"){
-      console.log('1');
-      if(operator === 'x'){
-        answer = new Fraction(parseInt(num1[0]) * num2 , parseInt(num1[1])).toString();
-      }else{
-        answer = new Fraction(parseInt(num1[0]) , (num2 * parseInt(num1[1]))).toString();
-      }
-    }else if(typeof num1 !== "object" && typeof num2 === "object"){
-      console.log('2');
-      if(operator === 'x'){
-        answer = new Fraction(parseInt(num2[0]) * num1 , parseInt(num2[1])).toString();
-      }else{
-        answer = new Fraction(parseInt(num2[0]) , (num1 * parseInt(num2[1]))).toString();
-      }
-    }else if(typeof num1 === "object" && typeof num2 === "object"){
-      console.log('3');
-      if(operator === 'x'){
-        answer = new Fraction((parseInt(num1[0]) * parseInt(num2[0])) , (parseInt(num1[1]) * parseInt(num2[1]))).toString();
-      }else{
-        answer = new Fraction((parseInt(num1[1]) * parseInt(num2[0])) , (parseInt(num1[0]) * parseInt(num2[1]))).toString();
-      }
+
+  console.log('0');
+
+  if(typeof num1 === "object" && typeof num2 !== "object"){
+    console.log('1');
+    if(operator === 'x'){
+      answer = new Fraction(parseInt(num1[0]) * num2 , parseInt(num1[1])).toString();
     }else if(operator === '/'){
-      console.log('4');
-      answer = num1 + '/' + num2
+      answer = new Fraction(parseInt(num1[0]) , (num2 * parseInt(num1[1]))).toString();
+    }else if(operator === '-'){
+      answer = new Fraction(parseInt(num1[0]) - num2 * parseInt(num1[1]) , parseInt(num1[1])).toString();
     }else{
-      answer = math_it_up[operator](num1,num2)
+      answer = new Fraction(num2 * parseInt(num1[1]) + parseInt(num1[0]), parseInt(num1[1])).toString();
     }
+  }else if(typeof num1 !== "object" && typeof num2 === "object"){
+    console.log('2');
+    if(operator === 'x'){
+      answer = new Fraction(parseInt(num2[0]) * num1 , parseInt(num2[1])).toString();
+    }else if(operator === '/'){
+      answer = new Fraction(parseInt(num2[0]) , (num1 * parseInt(num2[1]))).toString();
+    }else if(operator === '-'){
+      answer = new Fraction(num1 * parseInt(num2[1]) - parseInt(num2[0]), parseInt(num2[1])).toString();
+    }else{
+      answer = new Fraction(num1 * parseInt(num2[1]) + parseInt(num2[0]), parseInt(num2[1])).toString();
+    }
+  }else if(typeof num1 === "object" && typeof num2 === "object"){
+    console.log('3');
+    if(operator === 'x'){
+      answer = new Fraction((parseInt(num1[0]) * parseInt(num2[0])) , (parseInt(num1[1]) * parseInt(num2[1]))).toString();
+    }else if(operator === '/'){
+      answer = new Fraction((parseInt(num1[1]) * parseInt(num2[0])) , (parseInt(num1[0]) * parseInt(num2[1]))).toString();
+    }else if(operator === '-'){
+      answer = new Fraction(parseInt(num1[0]) * parseInt(num2[1]) - parseInt(num2[0]) * parseInt(num1[1]), parseInt(num2[1]) * parseInt(num1[1])).toString();
+    }else{
+      answer = new Fraction(parseInt(num1[0]) * parseInt(num2[1]) + parseInt(num2[0]) * parseInt(num1[1]), parseInt(num2[1]) * parseInt(num1[1])).toString();
+    }
+  }else if(operator === '/'){
+    console.log('4');
+    answer = new Fraction(parseInt(num1), parseInt(num2)).toString();
   }else{
-    console.log('5');
     answer = math_it_up[operator](num1,num2)
   }
-
-  console.log(answer);
-  return answer
+  console.log('answer top', answer, typeof answer);
+  if(typeof answer === "string"){
+    var split1 = answer.split(' ')
+    console.log('splt1', split1);
+    if(split1.length > 1){
+      var split2 = split1[1].split('/')
+      console.log('splt2', split2);
+      answer = (parseInt(split1[0]) * parseInt(split2[1]) + parseInt(split2[0])) +'/'+ parseInt(split2[1]);
+      console.log('answer bottom', answer);
+    }
+  }
+console.log(answer);
+return answer
 }
 
-// function fixFrac(num){
-//   var split = String(num).split('.');
-//   var
-//   var remainder = Math.floor(1 / Number(split[1])
-// }
 function setNums(item){
   var nums = dataFile[item].Solvables.split(' ');
   let array = [];
