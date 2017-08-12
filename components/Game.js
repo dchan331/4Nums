d695092eb09ec283ce76cc646270053dae8f48aa
dchan331 <dchan331@gmail.com>
2017-08-11 20:15:32 -0700

    finally works

--- components/Game.js
+++ components/Game.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, AsyncStorage } from 'react-native';
 import dataFile from './4Nums';
 import {Fraction} from 'fractional';
 
@@ -11,7 +11,9 @@ export default class Game extends React.Component {
       numbers: [],
       operators: ['+','-','x','/'],
       clicked: [],
-      solutions: []
+      solutions: [],
+      hist: [],
+      histIndex: 0
     }
     this.handleNum = this.handleNum.bind(this);
     this.handleSign = this.handleSign.bind(this);
@@ -30,7 +32,6 @@ export default class Game extends React.Component {
     var temp = this.state.clicked;
     var number = temp[temp.length - 1] ? temp[temp.length - 1][0] : 0;
     if(this.state.numbers.indexOf(number) !== -1){
-      console.log('in here');
       if(temp.length === 3){
         temp[2] = [this.state.numbers[num], num]
       }else{
@@ -45,17 +46,31 @@ export default class Game extends React.Component {
     }
 
     if(temp.length === 3){
-      console.log('in here');
+      console.log('state', this.state.numbers);
+      if(this.state.numbers.indexOf('') === -1){
+        console.log('in here');
+        AsyncStorage.setItem('hist', JSON.stringify([this.state.numbers]));
+      }
+      AsyncStorage.getItem('hist', (err, result) => {
+        console.log('in here 2');
+        var newResult = JSON.parse(result)
+        newResult.push(this.state.numbers)
+        console.log('NR', newResult);
+        this.setState({hist: newResult , 'histIndex': this.state.histIndex + 1})
+        AsyncStorage.setItem('hist', JSON.stringify(newResult));
+      })
+
+      console.log('bottom');
       var num1 = temp[0];
       var num2 = temp[2];
       var operator = temp[1];
       var newNumbers = this.state.numbers;
       newNumbers[num1[1]] = '';
       newNumbers[num2[1]] = calculator([num1[0], operator, num2[0]])
-      this.setState({numbers:newNumbers, clicked: []});
+      this.setState({clicked: [], numbers:newNumbers})
       if(checkWin(newNumbers) === 'winner'){
-        alert('Winner')
-        this.handleGame()
+        alert('Winner');
+        this.handleGame();
       }
     }
   }
@@ -81,7 +96,6 @@ export default class Game extends React.Component {
   }
 
   handleSolutions(x){
-    console.log('HS', this.state.item)
     if(x){
       this.setState({solutions: []})
     }else{
@@ -91,15 +105,19 @@ export default class Game extends React.Component {
   }
 
   handleUndo(){
-    var newClicked = this.state.clicked;
-    newClicked.pop();
-    this.setState({clicked: newClicked})
+    var newNums = this.state.hist[this.state.histIndex - 1]
+    this.setState({numbers: newNums, histIndex: this.state.histIndex - 1})
+  }
+  
+  handleRedo(){
+    var newNums = this.state.hist[this.state.histIndex + 1]
+    this.setState({numbers: newNums, histIndex: this.state.histIndex + 1})
   }
 
   render() {
-    // console.log('aaa', (new Fraction(12.5,0.5)).toString());
+    // console.log(this.state);
     return (
-      <View>
+      <View style={styles.entire}>
         {/* container for the 4 nums */}
         <View style={styles.container}>
           <View>
@@ -152,6 +170,10 @@ export default class Game extends React.Component {
   <View><Text>Undo </Text><Text>{this.state.clicked}</Text></View>
 </TouchableOpacity>
 
+<TouchableOpacity style={styles.square} onPress={() => this.handleRedo()}>
+  <View><Text>Redo </Text><Text>{this.state.clicked}</Text></View>
+</TouchableOpacity>
+
 <View style={styles.operatorsView}>
   {this.state.solutions.length !== 0 ?
     <View>
@@ -185,7 +207,7 @@ function calculator(array){
 
   var index1 = typeof array[len - 3] === "string" ? array[len - 3].split('/') : array[len - 3]
   var index2 = typeof array[len - 1] === "string" ? array[len - 1].split('/') : array[len - 1]
-  console.log('index', index1.length, index2.length);
+  // console.log('index', index1.length, index2.length);
   var num1 = typeof index1 === "object" && index1.length === 1 ? index1 : eval(index1)
   var num2 = typeof index2 === "object" && index2.length === 1 ? index2 : eval(index2);
   if(typeof num1 === "object" && num1.length === 1 ){
@@ -195,7 +217,7 @@ function calculator(array){
     num2 = num2[0];
   }
   let answer;
-  console.log('nums', num1, num2);
+  // console.log('nums', num1, num2);
   if(typeof num1 === "object" && typeof num2 !== "object"){
     console.log('1');
     if(operator === 'x'){
@@ -208,7 +230,7 @@ function calculator(array){
       answer = new Fraction(num2 * parseInt(num1[1]) + parseInt(num1[0]), parseInt(num1[1])).toString();
     }
   }else if(typeof num1 !== "object" && typeof num2 === "object"){
-    console.log('2');
+    // console.log('2');
     if(operator === 'x'){
       answer = new Fraction(parseInt(num2[0]) * num1 , parseInt(num2[1])).toString();
     }else if(operator === '/'){
@@ -219,7 +241,7 @@ function calculator(array){
       answer = new Fraction(num1 * parseInt(num2[1]) + parseInt(num2[0]), parseInt(num2[1])).toString();
     }
   }else if(typeof num1 === "object" && typeof num2 === "object"){
-    console.log('3');
+    // console.log('3');
     if(operator === 'x'){
       answer = new Fraction((parseInt(num1[0]) * parseInt(num2[0])) , (parseInt(num1[1]) * parseInt(num2[1]))).toString();
     }else if(operator === '/'){
@@ -230,7 +252,7 @@ function calculator(array){
       answer = new Fraction(parseInt(num1[0]) * parseInt(num2[1]) + parseInt(num2[0]) * parseInt(num1[1]), parseInt(num2[1]) * parseInt(num1[1])).toString();
     }
   }else if(operator === '/'){
-    console.log('4');
+    // console.log('4');
     answer = new Fraction(parseInt(num1), parseInt(num2)).toString();
   }else{
     answer = math_it_up[operator](parseInt(num1),parseInt(num2))
@@ -243,8 +265,8 @@ function calculator(array){
       answer = (parseInt(split1[0]) * parseInt(split2[1]) + parseInt(split2[0])) +'/'+ parseInt(split2[1]);
     }
   }
-console.log(answer);
-return answer
+  // console.log(answer);
+  return answer
 }
 
 function setNums(item){
@@ -286,7 +308,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   square:{
     backgroundColor: '#fff',
@@ -318,5 +340,10 @@ const styles = StyleSheet.create({
     width: 30,
     height: 30,
     borderWidth: 2
+  },
+  entire:{
+    flex:1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
   }
 });
